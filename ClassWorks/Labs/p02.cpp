27b242dcc397ff938435ec0bd1b067f3ea3dbaf1
Ismatov Shamsullo <ismatov_s@au.kg>
2018-10-05 13:53:51 +0600

    first commit

---
+++ ClassWorks/Labs/p02.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+main(){
+	
+	int s;
+	cin >> s;
+	
+	int d = 5;
+	
+	cout << s + d << endl;
+	cout << s + 2 * d << endl;
+	
+	return 0;
+}
+	
---
+++ ClassWorks/Labs/p03.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+	// multiple
+
+main() {
+	
+	int a; cin >> a;
+	int b; cin >> b;
+	
+	bool no = a % b;
+	
+	if(!no){
+		cout << "yes" << endl;
+	}else{
+		cout << "no" << endl;
+	}
+	
+	
+	
+	// 0 false 
+	// 1 true
+	
+	
+	
+}
+
+
