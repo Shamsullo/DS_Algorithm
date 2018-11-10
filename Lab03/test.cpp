6cd52e34e7fb3ce57c57989116fb79e3c8cd00ba
Shamsullo <shamsulloismatov@gmail.com>
2018-11-11 01:02:30 +0600

    some more additios

--- Lab03/test.cpp
+++ Lab03/test.cpp
@@ -1,21 +1,41 @@
 #include <iostream>
-#include <bits/stdc++.h>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <sstream>
 
 using namespace std;
 
+vector<string> split(const string& s, char delimiter)
+{
+   vector<std::string> tokens;
+   string token;
+   istringstream tokenStream(s);
+
+   while (getline(tokenStream, token, delimiter))
+   {
+      tokens.push_back(token);
+   }
+   return tokens;
+}
+string syllable(vector<string> ss){
+
+	string s = ss[ss.size() - 1];
+
+	for (int i = 0; i < s.size(); ++i)
+	{	
+		char c = s.at(i);
+		if (c == 'a' or c == 'e' or c == 'i' or c == 'o' or c == 'u'){
+			return s.substr(i, s.size());
+		}
+	}
+	return s;
+}
+
 int main()
 {
+    string line; getline(cin, line);
 
-    string n; getline(cin, n);
-    int numberOfLines = stoi(n);
-    
-    for(int i = 0 ; i < numberOfLines; i ++){
-
-    	string s;
-        getline(cin, s);
-
-        cout << s.find(' ') + 1 << "\n";
-    }
-
+     
     return 0;
 }
