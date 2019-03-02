60e9c2b09dbc406b41e5a49984f9cf314006c7ee
Shamsullo <34841262+Shamsullo@users.noreply.github.com>
2019-03-02 17:48:52 +0600

    Create 11062.cpp

---
+++ AlgorithmAnalysis/UVA/11062.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <set>
+#include <cctype>
+
+using namespace std;
+
+	// Andy's second dictionary
+
+int main(){
+
+	set<string> d;
+	
+	string line; 
+	
+	string result = ""; 
+
+	while (getline(cin, line)){
+
+		if(result[result.size() - 1] != '-'){
+			result = "";
+		}else{
+			result.erase(result.end() - 1);
+		} 
+
+		for (int i = 0; i < line.size(); ++i){	
+			
+			char ch = line[i];
+
+			if (isalpha(ch) or ch == '-'){
+				ch = tolower(ch);
+				result += ch;
+				if (i == line.size() -1 and ch != '-'){
+					d.insert(result);
+				}	
+			}else{
+				d.insert(result);
+				result = "";
+			}
+		}
+
+	}			
+	
+	for(auto s: d){	
+		if(!s.empty()){
+			cout << s << endl;
+		}
+	}
+
+	return 0;
+}
